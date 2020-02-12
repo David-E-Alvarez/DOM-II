0277eec33c338d46e236fec300c55b6be394ae75
david <davidealvarez1@gmail.com>
2020-02-12 06:46:41 -0800

    used mouseover and checked off task

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+//what do i want to do? I want to add a border around the little bus picture
+//step 1: get the pic 
+const picToAddBorderTo = document.querySelector('.intro img');
+//step 2: add add border
+picToAddBorderTo.addEventListener("mouseover", (event)=>{
+    event.target.style.border = "2px solid red";
+})
+//remove border after its already been over element
+picToAddBorderTo.addEventListener("mouseleave", (event)=>{
+    event.target.style.border = 'none';
+})
